ee82f7237b4b3e95f7adec7bf787e0452f492bec
PUSALA T R SATYA VANI <66248163+satya117@users.noreply.github.com>
2021-10-22 20:53:40 +0530

    Update p1.c

--- pattern/p1.c
+++ pattern/p1.c
@@ -1,1 +1,33 @@
+/*
 
+pattern 1
+-----------------
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    // 1.outer loop for rows
+    // 2.inner loop for compile
+    int row,col;
+    for(row=1;row<=n;row++){
+        for(col=1;col<=n;col++){
+            //what we need to print
+            printf("* ");
+        }
+        printf("\n");
+        
+    }
+    
+
+    return 0;
+}
